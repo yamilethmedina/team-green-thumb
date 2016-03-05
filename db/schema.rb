de5fcaff92fc0b3402406994aa2c9b4b61f0b1fe
Chris Scott <cyberstrike@gmail.com>
2016-03-05 12:22:19 -0500

    migrated

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20160305165121) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "activities", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
