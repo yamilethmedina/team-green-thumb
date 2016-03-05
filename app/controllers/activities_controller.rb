006252bdec280d4d4b1094c8a10b1a0405cf75ae
GoobleJook <robert@smallbizninja.com>
2016-03-05 14:58:14 -0500

    add activity form

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -69,6 +69,6 @@ class ActivitiesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def activity_params
-      params[:activity]
+      params.require(:activity).permit(:submission)
     end
 end
