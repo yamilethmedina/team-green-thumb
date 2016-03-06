ff53ff6adef6c8c6eb8bee2c7f9eb549dfbfb283
GoobleJook <robert@smallbizninja.com>
2016-03-05 19:14:13 -0500

    Add Paperclip to new badge

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,5 +1,11 @@
 class Badge < ActiveRecord::Base
-    
-    has_and_belongs_to_many :activities
-    
+  has_attached_file :badge_icon, styles: { 
+		thumb: '100x100',
+		square: '200x200#',
+		medium: '300x300>'
+	}
+	
+	validates_attachment_content_type :badge_icon, :content_type => /\Aimage\/.*\Z/
+  
+  has_and_belongs_to_many :activities    
 end
