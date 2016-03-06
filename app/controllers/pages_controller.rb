c838a296d467bf92ec81fb733f3ef8d296955e59
Chris Scott <cyberstrike@gmail.com>
2016-03-06 07:14:14 -0500

    styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def index
   end
   def dashboard
+    @badge = Badge.first
     @activities = Activity.all
     @activities_by_minutes = @activities.group_by &:minutes
   end
