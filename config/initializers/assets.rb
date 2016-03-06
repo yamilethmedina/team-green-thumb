95284fa394e6c7a7662bcfd7453435a8268ba670
Yami Medina <yamilethmedina1@gmail.com>
2016-03-06 13:52:06 +0000

    commit to get Chris the latest heroku version

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,5 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( favicon.ico )
 
 Rails.application.config.assets.precompile += %w( login_background.svg )
+
+Rails.application.config.assets.precompile += %w( green thumb logo.svg )
